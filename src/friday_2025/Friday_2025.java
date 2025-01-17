e6a900fec7434151e2a4c3f08b3f7e584e1f76d5
shiva <bandagor@sheridancollege.ca>
2025-01-17 16:36:25 -0500

    commit changes

---
+++ src/friday_2025/Friday_2025.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package friday_2025;
+
+/**
+ *
+ * @author shiva
+ */
+public class Friday_2025 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hi");
+        
+        //commit changes
+    }
+    
+}
